fa027b1aacf7a7207ba90dab7b141222c37791a1
bradshaw10 <eoghan.bradshaw@gmail.com>
2021-04-06 21:38:58 +0100

    css not connecting

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,18 +1,20 @@
-﻿var x = 0;
-var s = "";
+﻿$(document).ready(function () {
+    var x = 0;
+    var s = "";
 
-console.log("hello pluralsight");
+    console.log("hello pluralsight");
 
 
-var theForm = $("#theForm");
-theForm.hide();
+    var theForm = $("#theForm");
+    theForm.hide();
 
-var button = $("#buyButton");
-button.addEventListener("click", function () {
-    console.log("Buying Item");
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You click " + $(this).text());
+    });
 });
-
-var productInfo = $(".product-props li");
-productInfo.on("click", function () {
-    console.log("You click " + $(this).text());
-})
